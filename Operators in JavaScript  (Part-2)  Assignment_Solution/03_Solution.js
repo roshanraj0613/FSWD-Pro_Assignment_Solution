69f84997d84fe2eb5da3c685716a606bc92e5741
roshanraj0613 <rajroshan854105@gmail.com>
2023-12-02 03:07:53 +0530

    Operators in javascrip assignment solution updated

---
+++ Operators in JavaScript  (Part-2)  Assignment_Solution/03_Solution.js
@@ -0,0 +1,6 @@
+let number = 5; // Change this value to test with different numbers
+let result = (number > 0) ? 'positive' : (number < 0) ? 'negative' : 'zero';
+
+console.log(`The number is ${result}`);
+
+//output : The number is positive
---
+++ Operators in JavaScript  (Part-2)  Assignment_Solution/04_Solution.js
@@ -0,0 +1,6 @@
+let age = 20; // Change this value to test with different ages
+let eligibility = (age >= 18) ? 'You can vote' : 'You cannot vote';
+
+console.log(eligibility);
+
+//output : You can vote
